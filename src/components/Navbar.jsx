ca1574fd8f3bd5e4966e9bc8ba4ce838f68bc014
fatih <fthhbsr@gmail.com>
2022-07-30 14:13:31 +0300

    project completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,30 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="left-navbar">
+          <h1>
+            <span>Clarusway </span>Web Design
+          </h1>
+        </div>
+        <div className="right-navbar">
+          <ul className="nav-link ">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/services">Services</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,64 @@
+import htmllogo from "../assets/img/logo_html.png";
+import csslogo from "../assets/img/logo_css.png";
+import logobrush from "../assets/img/logo_brush.png";
+import Subscribe from "../components/Subscribe";
+import { useNavigate } from "react-router-dom";
+import { info } from "../assets/data";
+
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  console.log(info);
+  return (
+    <div>
+      <main>
+        <section className="banner">
+          <p id="name">Professional Web Design</p>
+          <p id="description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+            luctus ipsum, rhoncus semper magna. Nulla nec magna sit amet sem
+            interdum condimentum.
+          </p>
+        </section>
+        <Subscribe />
+        <section className="logo">
+          <div
+            className="box"
+            onClick={() => navigate("/html", { state: info[0].htmlInfo })}
+          >
+            <img src={htmllogo} alt="html_logo" />
+            <h3>HTML5 Markup</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus maxime perspiciatis reiciendis, asperiores
+              consequuntur eos
+            </p>
+          </div>
+          <div
+            className="box"
+            onClick={() => navigate("/css", { state: info[1].cssInfo })}
+          >
+            <img src={csslogo} alt="css_logo" />
+            <h3>CSS3 Styling</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+              mi augue, viverra sit amet ultricies
+            </p>
+          </div>
+          <div
+            className="box"
+            onClick={() => navigate("/logo", { state: info[2].logoInfo })}
+          >
+            <img src={logobrush} alt="graphic_logo" />
+            <h3>Graphic Design</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+              mi augue, viverra sit amet ultricies
+            </p>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 };
 
 export default Home;
